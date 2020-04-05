9c2234d68bcfd3fe29a99e6d8a870124e4acb718
Valdir Ruggeri <valdir.ruggeri@cloudmotion.com.br>
2020-04-05 19:04:11 -0300

    Add Project

---
+++ DemoArtifacts/MyLib.Nuget/LibInfo.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MyLib.Nuget
+{
+    public class LibInfo
+    {
+        public string VersionInfo
+        {
+            get { return "MyLib.Nuget info"; }
+        }
+    }
+}
